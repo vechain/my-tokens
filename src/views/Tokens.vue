937e1445e748e4d85993317287fe65894607944f
Asbert Ma <stayinalive@msn.cn>
2019-04-03 17:13:34 +0800

    token list

--- src/views/Tokens.vue
+++ src/views/Tokens.vue
@@ -1,8 +1,17 @@
 <template>
     <div style="padding: 20px 80px;">
-      <h1>Tokens</h1>
+        <h1>Tokens</h1>
         <a-row type="flex" justify="start">
-            <a-col :xs="24" :sm="12" :md="8" :lg="6" :xl="4" :xxl="3" v-for="(item, index) in tokenList" :key="index">
+            <a-col
+                :xs="24"
+                :sm="12"
+                :md="8"
+                :lg="6"
+                :xl="4"
+                :xxl="3"
+                v-for="(item, index) in tokenList"
+                :key="index"
+            >
                 <token-card :item="item"></token-card>
             </a-col>
         </a-row>
@@ -13,7 +22,12 @@ import { Vue, Component } from 'vue-property-decorator'
 @Component
 export default class Tokens extends Vue {
     get tokenList() {
-        return this.$store.state.tokens
+        return (this.$store.getters.tokens as app.Token[]).map((item) => {
+            return {
+                ...item,
+                img: require(`../assets/${item.icon}`)
+            }
+        })
     }
 }
 </script>
