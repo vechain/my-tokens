a5f74efeda318b89b874f1b8aab67fdb37f41412
Asbert Ma <stayinalive@msn.cn>
2019-04-03 17:16:31 +0800

    wallet detail

--- src/views/WalletDetail.vue
+++ src/views/WalletDetail.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>
-    detail
-  </div>
+    <div>detail</div>
 </template>
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 @Component
 export default class WalletDetail extends Vue {
+    public wallet: app.Wallet | null = null
+
+    public created() {
+        const address = this.$route.params.id
+        this.wallet = (this.$store.getters.wallets as app.Wallet[]).find((item) => {
+            return item.address === address
+        }) || null
+        // if (this.wallet && this.wallet.own) {
+        //     this.walletInfoCheck(address)
+        // }
+    }
+
+    // public walletInfoCheck(addr: string) {
+    //     if (!connex.vendor.owned(addr)) {
+    //         this.$store.dispatch('walletOwnUpdate', {
+    //             addr,
+    //             isOwn: false
+    //         })
+    //     }
+    // }
 }
 </script>
