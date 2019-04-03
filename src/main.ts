2289c0638dbb26f45c5b2020484598a31b0562d1
Asbert Ma <stayinalive@msn.cn>
2019-04-03 17:10:43 +0800

    add start function

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
 import Vue from 'vue'
+import Store from './store'
 import App from './App.vue'
 import router from './router'
-import Store from './store'
 import './i18n'
 import './antd'
 import './components'
 import './filters'
+import './directives'
 import 'ant-design-vue/dist/antd.css'
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store: new Store(),
-  render: (h) => h(App),
-}).$mount('#app')
+async function createApp() {
+  await Store.initState()
+  new Vue({
+    router,
+    store: Store,
+    render: (h) => h(App)
+  }).$mount('#app')
+}
+createApp()
