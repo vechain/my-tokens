19dbc7f08fa7802e4f266e0b2c23a4151b7dc961
Asbert Ma <stayinalive@msn.cn>
2019-04-11 20:01:14 +0800

    components

---
+++ src/components/TokenBalanceCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <a-card>
+        <a-row type="flex">
+            <a-col :xs="2">
+                <img width="50" :src="item.img" :alt="item.name">
+            </a-col>
+            <a-col :xs="12">
+                <span>{{item.name}}</span>
+                <p>{{item.balance}}</p>
+            </a-col>
+            <a-col :xs="4">
+                <router-link
+                    class="go-transfer"
+                    :to="{name: 'transfer', query: {
+                    symbol: item.symbol
+                }}"
+                >Transfer</router-link>
+            </a-col>
+        </a-row>
+    </a-card>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+@Component
+export default class TokenBalanceCard extends Vue {
+    @Prop(Object)
+    private item!: app.Balance & app.Token
+}
+</script>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import NavBar from './NavBar.vue'
 import TokenCard from './TokenCard.vue'
+import TokenBalanceCard from './TokenBalanceCard.vue'
 import WalletCard from './WalletCard.vue'
+import QRCode from './QRCode.vue'
 
 Vue.component(NavBar.name, NavBar)
 Vue.component(TokenCard.name, TokenCard)
+// tslint:disable-next-line:no-console
+console.log(TokenBalanceCard.name)
+Vue.component(TokenBalanceCard.name, TokenBalanceCard)
 Vue.component(WalletCard.name, WalletCard)
+Vue.component(QRCode.name, QRCode)
