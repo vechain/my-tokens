638d2d6b93fa41408489de924474f69d8ed086e0
Asbert Ma <stayinalive@msn.cn>
2019-04-11 20:02:30 +0800

    add balance model

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,20 +1,24 @@
 // tslint:disable-next-line:no-namespace
 declare namespace app {
   interface Wallet {
-    name: string,
-    address: string,
-    own: boolean,
+    name: string
+    address: string
+    own?: boolean
     id?: number
   }
 
   interface Token {
-    id?: number,
-    name: string,
-    symbol: string,
-    icon: string,
-    desc: string,
-    address: string,
-    url?: string,
+    id?: number
+    name: string
+    symbol: string
+    icon: string
+    desc: string
+    address: string
+    url?: string
     decimals: number
+  }
+
+  interface Balance {
+    balance: number
   }
 }
