79051ca447a0e28811f12e6bccad1f0b840456b4
Asbert Ma <stayinalive@msn.cn>
2019-04-12 18:57:04 +0800

    detail page

--- src/views/WalletDetail.vue
+++ src/views/WalletDetail.vue
@@ -1,29 +1,100 @@
 <template>
     <div class="wallet-detail">
-        <a-row type="flex">
-            <a-col :xs="8">
-                <QRCode :content="wallet.address" :size="100"/>
-            </a-col>
-            <a-col :xs="16">
-                <a-card >
-                    <TokenBalanceCard style="margin-top: 10px" :item="item" v-for="item in tokenlist" :key="item.symbol"/>
-                </a-card>
+        <a-row type="flex" justify="center">
+            <a-col>
+                <a-row :gutter="{xs: 8, lg: 24}" type="flex" justify="space-around">
+                    <a-col :xs="8" :lg="6">
+                        <a-row>
+                            <a-col>
+                                <a-row type="flex" :gutter="8" justify="center" align="middle">
+                                    <a-col :xs="6">
+                                        <div
+                                            style="width: 50px; height: 50px; margin: auto; border-radius: 25px; display: inline-block"
+                                            v-picasso="wallet.address"
+                                        ></div>
+                                    </a-col>
+                                    <a-col :xs="15" class="wallet-name">
+                                        <a-input
+                                            v-show="isEdit"
+                                            size="large"
+                                            ref="input"
+                                            @keypress.enter="saveName"
+                                            @blur="isEdit=false"
+                                            v-model="name"
+                                        ></a-input>
+                                        <span v-show="!isEdit">{{wallet.name}}</span>
+                                        <a-button
+                                            v-show="!isEdit"
+                                            style="margin-left: 20px"
+                                            shape="circle"
+                                            icon="edit"
+                                            @click="showEdit"
+                                        />
+                                    </a-col>
+                                </a-row>
+                            </a-col>
+                            <a-col style="margin-top: 30px">
+                                <a-card>
+                                    <QRCode
+                                        style="margin: auto"
+                                        :content="wallet.address"
+                                        :size="220"
+                                    />
+                                </a-card>
+                            </a-col>
+                            <a-col>
+                                <a-row
+                                    type="flex"
+                                    :gutter="8"
+                                    justify="space-around"
+                                    align="middle"
+                                >
+                                    <a-col :xs="18">
+                                        <p ref="address" class="wallet-address">{{wallet.address}}</p>
+                                    </a-col>
+                                    <a-col :xs="4">
+                                        <a-button @click="copy" shape="circle" icon="copy"/>
+                                    </a-col>
+                                </a-row>
+                            </a-col>
+                        </a-row>
+                    </a-col>
+                    <a-col :xs="14" :lg="16">
+                        <!-- <a-card class="token-balance-list"> -->
+                        <a-row type="flex" :gutter="8">
+                            <a-col :xs="24" :lg="12" v-for="item in tokenlist" :key="item.symbol">
+                                <TokenBalanceCard :item="item" style="margin-top: 10px"/>
+                            </a-col>
+                        </a-row>
+                        <!-- </a-card> -->
+                    </a-col>
+                </a-row>
             </a-col>
         </a-row>
     </div>
 </template>
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component, Watch } from 'vue-property-decorator'
 @Component
 export default class WalletDetail extends Vue {
-    public wallet: app.Wallet | null = null
+    // public wallet: app.Wallet | null = null
     public qrcodeHtml?: string
+    public name: string = ''
+    public isEdit = false
+
+    @Watch('wallet')
+    public walletChange(newVal: app.Wallet) {
+        this.name = newVal.name
+    }
 
     public created() {
-        const address = this.$route.params.id
-        this.wallet = (this.$store.getters.wallets as app.Wallet[]).find((item) => {
-            return item.address === address
-        }) || null
+        this.name = this.wallet.name
+    }
+
+    get wallet() {
+        return this.$store.getters.wallets.find((item: app.Wallet) => {
+            return item.address === this.$route.params.id
+        })
     }
 
     get tokenlist() {
@@ -35,6 +106,7 @@ export default class WalletDetail extends Vue {
             }
         })
     }
+
     get balances() {
         if (this.$store.getters.balanceList && this.wallet && this.$store.getters.balanceList[this.wallet!.address]) {
             return this.$store.getters.balanceList[this.wallet!.address]
@@ -42,11 +114,50 @@ export default class WalletDetail extends Vue {
             return {}
         }
     }
+
+    public async saveName() {
+        this.isEdit = false
+        await this.$store.dispatch('updateWallet', {
+            address: this.wallet!.address,
+            name: this.name
+        })
+    }
+
+    public showEdit() {
+        this.isEdit = true
+        const ele = this.$refs.input as HTMLInputElement
+        this.$nextTick(() => {
+            ele.focus()
+            ele.select()
+        })
+    }
+
+    public copy() {
+        // (this.$refs['address'] as HTMLParagraphElement).select()
+    }
 }
 </script>
 <style>
 .wallet-detail {
-    max-width: 1080px;
+    max-width: 1200px;
     margin: auto;
+    margin-top: 40px;
+}
+
+.token-balance-list {
+    border-radius: 5px;
+}
+.wallet-name {
+    text-align: left;
+    font-size: 20px;
+}
+.wallet-address {
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    display: -webkit-box;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: normal;
+    word-break: break-all;
 }
 </style>
