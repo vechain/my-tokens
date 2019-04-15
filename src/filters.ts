1ec38d60630164fd65a882e51b2eed19b5de60f1
Asbert Ma <stayinalive@msn.cn>
2019-04-15 18:04:20 +0800

    balance filter

--- src/filters.ts
+++ src/filters.ts
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 
 Vue.filter('shortAddress', (v: string) => {
-  return v.substring(0, 6) + '......' + v.substring(v.length - 5, v.length - 1)
+  return v.substring(0, 6) + '......' + v.substring(v.length - 5, v.length)
+})
+
+Vue.filter('balance', (val: number) => {
+  return Number(val).toLocaleString()
 })
