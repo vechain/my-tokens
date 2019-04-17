6a8ed4dcda5ac4dc06a7f99cd95f432184f72e08
Asbert Ma <stayinalive@msn.cn>
2019-04-17 11:40:48 +0800

    add test net token

--- src/tokens.ts
+++ src/tokens.ts
@@ -64,5 +64,54 @@ export const testNetTokens = [
     address: '0x0000000000000000000000000000456e65726779',
     decimals: 18,
     icon: 'vtho.png'
+  },
+  {
+    name: 'OceanEx Token',
+    symbol: 'OCE',
+    address: '0x9652aead889e8df7b5717ed984f147c132f85a69',
+    decimals: 18,
+    icon: 'oce.png'
+  },
+  {
+    name: 'Yeet Coin',
+    symbol: 'YEET',
+    address: '0x32456c328f647f5b35757d38fe634868d9fe3808',
+    decimals: 18,
+    icon: 'yeet.png'
+  },
+  {
+    name: 'TicTalk Token',
+    symbol: 'TIC',
+    address: '0x18081982d752ADd4f402AEAe480d6c90f7644427',
+    decimals: 18,
+    icon: 'tic.png'
+  },
+  {
+    name: 'Plair',
+    symbol: 'PLA',
+    address: '0x645d2019ed39e58db76af602317d177b53ba8b9d',
+    decimals: 18,
+    icon: 'pla.png'
+  },
+  {
+    name: 'Safe Haven Token',
+    symbol: 'SHA',
+    address: '0x9c6e62b3334294d70c8e410941f52d482557955b',
+    decimals: 18,
+    icon: 'sha.png'
+  },
+  {
+    name: 'Decent.bet Token',
+    symbol: 'DBET',
+    address: '0x510fcddc9424b1bbb328a574f45bfddb130e1f03',
+    decimals: 18,
+    icon: 'dbet.png'
+  },
+  {
+    name: 'Bitarora Token',
+    symbol: 'BAG',
+    address: '0x4D9a463b28284228Ce260887F7D0C3A63a8150b9',
+    decimals: 18,
+    icon: 'bag.png'
   }
 ]
