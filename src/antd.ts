14dea256ac21ebb2ba7e548993d29aff72730941
Asbert Ma <stayinalive@msn.cn>
2019-04-19 11:53:58 +0800

    import the components needed

--- src/antd.ts
+++ src/antd.ts
@@ -1,5 +1,59 @@
 import Vue from 'vue'
-import Antd from 'ant-design-vue'
+import {
+  Form,
+  Input,
+  Layout,
+  Row,
+  Icon,
+  Col,
+  Select,
+  Button,
+  Tooltip,
+  Modal,
+  Menu,
+  Card
+} from 'ant-design-vue'
 
-Vue.use(Antd)
+Vue.component(Card.name, Card)
+Vue.use(Card)
 
+Vue.component(Menu.name, Menu)
+Vue.component(Menu.Item.name, Menu.Item)
+Vue.component(Menu.ItemGroup.name, Menu.ItemGroup)
+Vue.use(Menu)
+
+Vue.component(Form.name, Form)
+Vue.component(Form.Item.name, Form.Item)
+Vue.use(Form)
+
+Vue.component(Input.name, Input)
+Vue.component(Input.Group.name, Input.Group)
+Vue.use(Input)
+
+Vue.component(Layout.name, Layout)
+Vue.component(Layout.Header.name, Layout.Header)
+Vue.component(Layout.Content.name, Layout.Content)
+Vue.component(Layout.Footer.name, Layout.Footer)
+Vue.use(Layout)
+
+Vue.component(Row.name, Row)
+Vue.component(Icon.name, Icon)
+Vue.component(Col.name, Col)
+Vue.use(Row)
+Vue.use(Icon)
+Vue.use(Col)
+
+Vue.component(Select.name, Select)
+Vue.component(Select.Option.name, Select.Option)
+Vue.component(Select.OptGroup.name, Select.OptGroup)
+Vue.use(Select)
+
+Vue.component(Button.name, Button)
+Vue.component(Button.Group.name, Button.Group)
+Vue.use(Button)
+
+Vue.component(Tooltip.name, Tooltip)
+Vue.use(Tooltip)
+
+Vue.component(Modal.name, Modal)
+Vue.use(Modal)
