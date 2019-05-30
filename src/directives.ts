6c0292a1e6d309f7cce06bdc3a4315bdeed00163
Asbert Ma <stayinalive@msn.cn>
2019-05-30 10:33:03 +0800

    update svg on content change

--- src/directives.ts
+++ src/directives.ts
@@ -1,12 +1,14 @@
-import Vue from 'vue'
+import Vue, { VNode } from 'vue'
 import { picasso } from '@vechain/picasso'
-Vue.directive('picasso', {
-  inserted(el, binding) {
-    const svg = picasso((binding.value as string).toLowerCase())
-    if (el.tagName === 'IMG') {
-      (el as HTMLImageElement).src = `data:image/svg+xml;utf8,${svg}`
-    } else {
-      (el as HTMLElement).style.background = `no-repeat url('data:image/svg+xml;utf8,${svg}')`
-    }
+Vue.directive('picasso', (el, binding) => {
+  if (binding.value === binding.oldValue) {
+    return
+  }
+
+  const svg = picasso((binding.value as string).toLowerCase())
+  if (el.tagName === 'IMG') {
+    (el as HTMLImageElement).src = `data:image/svg+xml;utf8,${svg}`
+  } else {
+    (el as HTMLElement).style.background = `no-repeat url('data:image/svg+xml;utf8,${svg}')`
   }
 })
