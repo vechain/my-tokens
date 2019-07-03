c6afde2c4453f9cd2526084d0dc3889dd8a7dbcd
Asbert Ma <stayinalive@msn.cn>
2019-07-03 18:00:38 +0800

    languages

---
+++ src/lang/SimplifiedChinese.ts
@@ -0,0 +1,48 @@
+ export default {
+  navBar: {
+    wallets: '钱包',
+    transfer: '转账'
+  },
+  wallets: {
+    title: '钱包',
+    import: '导入',
+    add: '添加',
+    observe: '观察',
+    title_add: '添加钱包',
+    or: '或',
+    detail: '详情',
+    transfer: '转账',
+    delete: '移除',
+    copied: '已复制',
+    hide_amount: '隐藏小额资产',
+    addr_placeholder: '地址为0x开头',
+    token: '币种',
+    select_wallet: '点击选择钱包',
+    unknow: '未知钱包',
+    remove_msg: '是否要移除钱包?',
+    remove_yes: '是',
+    remove_no: '否',
+    remove_title: '移除钱包',
+    first_time: '使用前请先添加钱包'
+  },
+  transfer: {
+    from: '转出',
+    to: '转入',
+    amount: '数额',
+    title: '转账',
+    send: '发送',
+    no_wallet: '未检测到钱包，您需要先导入',
+    import_after_tx: '交易发出后将钱包导入',
+    tx_success: '交易已发送',
+    tx_info : '交易哈希为: {txid},点击查看详情'
+  },
+  msg: {
+    addr_require: '地址为必填项',
+    addr_format: '地址格式错误',
+    from_require: '转出地址未输入',
+    to_format: '转入地址格式错误',
+    to_require: '转入地址未输入',
+    amount_require: '转出数额未输入',
+    amount_invalid: '转出数额格式错误'
+  }
+}
---
+++ src/lang/TraditionalChinese.ts
@@ -0,0 +1,47 @@
+export default {
+  navBar: {
+    wallets: '錢包',
+    transfer: '轉賬'
+  },
+  wallets: {
+    title: '錢包',
+    import: '導入',
+    add: '添加',
+    observe: '觀察',
+    title_add: '添加錢包',
+    or: '或',
+    detail: '詳情',
+    transfer: '轉賬',
+    delete: '移除',
+    copied: '已複製',
+    hide_amount: '隱藏小額資產',
+    addr_placeholder: '地址為0x開頭',
+    token: '幣種',
+    select_wallet: '點擊選擇錢包',
+    unknow: '未知錢包',
+    remove_msg: '是否要移除錢包?',
+    remove_yes: '是',
+    remove_no: '否',
+    remove_title: '移除錢包'
+  },
+  transfer: {
+    from: '轉出',
+    to: '轉入',
+    amount: '數額',
+    title: '轉賬',
+    send: '發送',
+    no_wallet: '未檢測到錢包，您需要先導入',
+    import_after_tx: '交易發出後將錢包導入',
+    tx_success: '交易已發送',
+    tx_info : '交易哈希為: {txid},點擊查看詳情'
+  },
+  msg: {
+    addr_require: '地址為必填項',
+    addr_format: '地址格式錯誤',
+    from_require: '轉出地址未輸入',
+    to_format: '轉入地址格式錯誤',
+    to_require: '轉入地址未輸入',
+    amount_require: '轉出數額未輸入',
+    amount_invalid: '轉出數額格式錯誤'
+  }
+}
