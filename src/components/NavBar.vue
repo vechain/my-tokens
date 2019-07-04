b389a30dbda5ea69ed94104d237290cd0d08babe
Asbert Ma <stayinalive@msn.cn>
2019-07-04 16:44:56 +0800

    language switch

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
     <a-row type="flex" justify="space-between">
-        <a-col :span="12">
+        <a-col :span="8">
             <a-row type="flex" justify="start">
                 <div class="logo">VeChain</div>
             </a-row>
@@ -19,14 +19,34 @@
                 </a-col>
             </a-row>
         </a-col>
+        <a-col :span="3">
+            <a-row type="flex" align="bottom" justify="space-around">
+                <a-col :span="16">
+                    <a-select
+                        class="select-re-theme"
+                        dropdownClassName="select-list-rebg"
+                        style="width: 100px"
+                        v-model="$i18n.locale"
+                        defaultValue="en"
+                    >
+                        <a-select-option
+                            v-for="item in langs"
+                            :key="item.value"
+                            :value="item.value"
+                        >{{item.text}}</a-select-option>
+                    </a-select>
+                </a-col>
+            </a-row>
+        </a-col>
     </a-row>
 </template>
 <script lang="ts">
 import { Vue, Component, Watch } from 'vue-property-decorator'
+import { langList } from '../lang'
 @Component
 export default class NavBar extends Vue {
     public current: any[] = []
-
+    public langs = langList
     @Watch('$route')
     public routed() {
         this.current = [this.$route.meta.key]
--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,4 +1,24 @@
 import en from './en'
+import zh_TW from './TraditionalChinese'
+import zh_CN from './SimplifiedChinese'
+
 export default {
-  en
+  en,
+  zh_TW,
+  zh_CN
 }
+
+export const langList = [
+  {
+    text: 'English',
+    value: 'en'
+  },
+  {
+    text: '繁體中文',
+    value: 'zh_TW'
+  },
+  {
+    text: '简体中文',
+    value: 'zh_CN'
+  }
+]
