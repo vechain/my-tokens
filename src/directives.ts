b06b616ae9928123f4af351f830f3628005eab85
Asbert Ma <stayinalive@msn.cn>
2020-11-05 13:44:57 +0800

    picasso upgrade

--- src/directives.ts
+++ src/directives.ts
@@ -9,6 +9,8 @@ Vue.directive('picasso', (el, binding) => {
   if (el.tagName === 'IMG') {
     (el as HTMLImageElement).src = `data:image/svg+xml;utf8,${svg}`
   } else {
-    (el as HTMLElement).style.background = `no-repeat url('data:image/svg+xml;utf8,${svg}')`
+    (el as HTMLElement).setAttribute(
+      'style',
+      `background-image:url('data:image/svg+xml;utf8,${svg}'); background-repeat: no-repeat; background-size: contain`)
   }
 })
